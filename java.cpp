4dbecd579b7abac4fcdd3d161f00da639193dad4
Gaurav <gauravajariwal.951@gmail.com>
2019-01-03 00:44:57 +0530

    debugged java.cpp file

--- java.cpp
+++ java.cpp
@@ -1,4 +1,4 @@
 #include "java.h"
 
 Java::Java(std::string name, double price, double cost, int darkness) : Product{name, price, cost}, _darkness{0} { }
-Java::add_shot(Shot shot) {return _shots.pushback(shot)}
+void Java::add_shot(Shot shot) {return _shots.push_back(shot);}
