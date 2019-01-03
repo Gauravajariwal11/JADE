1da28233027fa9563045a58d1ef31895c486205a
Gaurav <gauravajariwal.951@gmail.com>
2019-01-04 00:33:14 +0530

    added store.cpp

---
+++ store.cpp
@@ -0,0 +1,7 @@
+#include "store.h"
+
+Store::Store(std::string store_name) : _name{store_name} { }
+std::string Store::name() {return _name;}
+void Store::add_product(Product* product) { _products.push_back(product); }
+int Store::number_of_products() {return _products.size();}
+std::string Store::product_to_string(int product) { std::cout<< _products[product]-> to_string() << std::endl; } 
