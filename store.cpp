a074b28fae057391857babc08fffe2dd20596049
Gaurav <gauravajariwal.951@gmail.com>
2019-01-05 20:14:18 +0530

    made changes to store.cpp

--- store.cpp
+++ store.cpp
@@ -4,4 +4,12 @@ Store::Store(std::string store_name) : _name{store_name} { }
 std::string Store::name() {return _name;}
 void Store::add_product(Product* product) { _products.push_back(product); }
 int Store::number_of_products() {return _products.size();}
-std::string Store::product_to_string(int product) { std::cout<< _products[product]-> to_string() << std::endl; } 
+std::string Store::product_to_string(int product) { std::cout<< _products[product]-> to_string() << std::endl; }
+
+std::ostream& operator<<(std::ostream& ost, Store& store) {
+	ost << "Store " << store._name << std::endl << std::endl;
+	ost<< "Products: " << std::endl;
+
+	for (auto p : store._products) {ost << " " << p->to_string() << std::endl;}
+		return ost;
+} 
