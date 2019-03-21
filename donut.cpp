d65501ad1db67025099f5e44f6867fd604e7d571
Gaurav <gauravajariwal.951@gmail.com>
2019-03-21 13:28:39 -0500

    Finished donut.h and donut.cpp files

--- donut.cpp
+++ donut.cpp
@@ -1,41 +1,22 @@
 #include "donut.h"
 
-Donut::Donut(std::string name,double price, double cost, Frosting frosting, bool sprinkles, Filling filling) 
-	: Product{name, price, cost}, _frosting{frosting}, _sprinkles{false}, _filling{filling} {}
-
-std::string Donut::frosting_to_string(Frosting frost) {
-	if(frost == Frosting::UNFROSTED) {return "Unfrosted";}
-	else if (frost == Frosting::CHOCOLATE_TOP) {return "Chocolate_Top";}
-	else if (frost == Frosting::VANILLA_TOP) {return "Vanilla_Top";}
-	else {return "Pink_Top";}
-}
-
-std::string Donut::filling_to_string(Filling fill){
-	if (fill == Filling::UNFILLED) return "Unfilled";
-	else if (fill == Filling::CREME) return "Creme";
-	else if (fill == Filling::BAVARIAN) return "Bavarian";
-	else return "Strawberry";
-}
+Donut::Donut(std::string name, double price, double cost, Frosting frosting, bool sprinkles, Filling filling)
+    : Product{name, price, cost}, _frosting{frosting}, _sprinkles{sprinkles}, _filling{filling} { }
 
 std::string Donut::to_string() {
-
-	std::string result = Product::to_string();
-	std::string separator = "(";
-	std::string terminator = ".";
-
-	if(_frosting != UNFROSTED) {
-		result += separator + "frosted with " + Donut::frosting_to_string(_frosting);
-		if (_sprinkles) result += " and sprinkles";
-		separator = ", ";
-		terminator = ")"; 
-	}
-
-	if (_filling != UNFILLED){
-		result += separator + "filled with " + Donut::filling_to_string(_filling);
-		terminator = ")";
-	}
-
-	result += terminator;
-	return result;
-
+    std::string result = Product::to_string();
+    std::string separator = " (";
+    std::string terminator = "";
+    if (_frosting != Unfrosted) {
+        result += separator + "frosted with " + frosting_to_string[_frosting];
+        if (_sprinkles) result += " and sprinkles";
+        separator = ", ";
+        terminator = ")";
+    }
+    if (_filling != Unfilled) {
+        result += separator + "filled with " + filling_to_string[_filling];
+        terminator = ")";
+    }
+    result += terminator;
+    return result;
 }
--- donut.h
+++ donut.h
@@ -1,22 +1,22 @@
-#pragma once 
+#pragma once
 #include "product.h"
-#include <string>
-#include <iostream>
+#include <vector>
+#include <ostream>
 
-enum Frosting{UNFROSTED, CHOCOLATE_TOP, VANILLA_TOP, PINK_TOP};
-enum Filling{UNFILLED, CREME, BAVARIAN, STRAWBERRY};
+enum Frosting {Unfrosted, Chocolate_top, Vanilla_top, Pink_top};
+const std::vector<std::string> frosting_to_string =
+    {"Unfrosted", "Chocolate", "Vanilla", "Pink"};
 
+enum Filling {Unfilled, Creme, Bavarian, Strawberry};
+const std::vector<std::string> filling_to_string =
+    {"Unfilled", "Creme", "Bavarian", "Strawberry"};
 
 class Donut : public Product {
-
-protected:
-	Frosting _frosting;
-	bool _sprinkles;
-	Filling _filling;
-
-public:
-	Donut(std::string name,double price, double cost, Frosting frosting, bool sprinkles, Filling filling);
-	std::string to_string() override; 
-	std::string frosting_to_string (Frosting frost);
-	std::string filling_to_string(Filling fill);
+  public:
+    Donut(std::string name, double price, double cost, Frosting frosting, bool sprinkles, Filling filling);
+    std::string to_string() override;
+  protected:
+    Frosting _frosting;
+    bool _sprinkles;
+    Filling _filling;
 };
