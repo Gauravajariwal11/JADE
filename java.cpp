f14382ee4de544fcf37a91c1fb02f37c0e37e9ad
Gaurav <gauravajariwal.951@gmail.com>
2019-03-21 13:29:57 -0500

    finished updating java.h and java.cpp files

--- java.cpp
+++ java.cpp
@@ -1,31 +1,14 @@
 #include "java.h"
 
-Java::Java(std::string name, double price, double cost, int darkness) : Product{name, price, cost}, _darkness{darkness} { }
+Java::Java(std::string name, double price, double cost, Darkness darkness)
+    : Product{name, price, cost}, _darkness{darkness} { }
+
 void Java::add_shot(Shot shot) {_shots.push_back(shot);}
-std::string Java::shots_to_string(Shot sts) {
-	if (sts == Shot::None) return "None";
-	else if (sts == Shot::Chocolate) return "Chocolate";
-	else if (sts == Shot::Vanilla) return "Vanilla";
-	else if (sts == Shot::Peppermint) return "Peppermint";
-	else return "Hazelnut";
-}
-std::string Java::Darkness_to_string(int _darkness) {
-	if (_darkness == 1) return "Blonde"; 
-	else if (_darkness == 2) return "Light";
-	else if (_darkness == 3) return "Medium";
-	else if (_darkness == 4) return "Dark";
-	else return "Extra Dark";
 
-}
-
-std::string Java::to_string() { 
-	std::string result = Product::to_string() + " (" + Java::Darkness_to_string(_darkness) + ") with ";
-
-
-//	result += std::to_string(_darkness);
-	for (auto i : _shots) { result += Java::shots_to_string(i) + " "; }
-
-	result += ".";
-
-	return result;
+std::string Java::to_string() {
+    std::string result =  Product::to_string() + " (" + darkness_to_string[_darkness]
+        + ")";
+    std::string separator = " with ";
+    for (auto s : _shots) {result += separator + shot_to_string[s]; separator = ", ";}
+    return result;
 }
--- java.h
+++ java.h
@@ -1,24 +1,23 @@
 #pragma once
 #include "product.h"
-#include <iostream>
-#include <string>
 #include <vector>
+#include <ostream>
 
-enum Shot{None, Chocolate, Vanilla, Peppermint, Hazelnut};
+enum Darkness {Blonde, Light, Medium, Dark, Extra_dark};
+const std::vector<std::string> darkness_to_string =
+        {"Blonde", "Light", "Medium", "Dark", "Extra Dark"};
 
-const int DARK_LEVELS = 5;
+enum Shot {None, Chocolate, Vanilla, Peppermint, Hazelnut, Caramel};
+const std::vector<std::string> shot_to_string =
+    {"None", "Chocolate", "Vanilla", "Peppermint", "Hazelnut", "Caramel"};
 
 class Java : public Product {
-
-protected:
-	int _darkness;
-	std::vector<Shot> _shots;
-
-public:
-	Java(std::string name, double price, double cost, int darkness);
-	void add_shot(Shot shot);
-	std::string shots_to_string (Shot sts);
-	std::string Darkness_to_string(int _darkness);
-	std::string to_string() override;
-
+  public:
+    Java(std::string name, double price, double cost, Darkness darkness);
+    void add_shot(Shot shot);
+    std::string to_string() override;
+  protected:
+    Darkness _darkness;
+    std::vector<Shot> _shots;
 };
+    
